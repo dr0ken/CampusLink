c3cacce23153cd67c34f7360c351c675b5b86aac
dr0ken <timastepanov65@gmail.com>
2025-09-20 15:52:53 +0500

    Create branch

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+
