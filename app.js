5da24db4ccdfbcf3851d7e35ce0a7e543c7760ab
dr0ken <timastepanov65@gmail.com>
2025-09-20 16:12:26 +0500

    add db connection

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 import express from 'express'
 import config from 'config'
+import mongoose from 'mongoose'
 
 const app = express()
 
 const PORT = config.get('port') || 5000
 
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+async function start() 
+{
+    try 
+    {
+        await mongoose.connect(config.get("mongoUri"))
+
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    } 
+    catch (e) 
+    {
+        console.log('Server error', e.message)
+        process.exit(1);
+    }
+}
+
+
+start()
+
+
 
